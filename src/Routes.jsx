243ee95b5f355acec7fc652b744891f94a743c6e
Swathi-M-D <swathimd2010@gmail.com>
2025-04-09 18:49:28 +0530

    1st-commit

---
+++ src/Routes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegistrationPage from "./RegistrationPage";
+import CustomerHomePage from "./CustomerHomePage";
+import CartPage from "./CartPage";
+import OrderPage from "./OrderPage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="/customerhome" element={<CustomerHomePage />} />
+      <Route path="/UserCartPage" element={<CartPage />} />
+      <Route path="/orders" element={<OrderPage />} />
+      
+    </Routes>
+  );
+};
+
+export default AppRoutes;
